3e98651c59ea69cf3c1896e16a25aac4da797a86
EdRafVasD <rafavas99@gmail.com>
2024-03-13 22:17:49 -0500

    agregando formulario

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ menu_responsive.onclick = function () {
   let navBar = document.querySelector(".menu");
   navBar.classList.toggle("active");
 };
+
+// para el formulario
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+const alertaContainer = document.getElementById("alerta-container");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 caracteres`;
+    valido = false;
+  }
+
+  parrafo.innerHTML = valido ? "Enviado" : warnings;
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    alertaContainer.classList.add('oculto');
+    formulario.submit();
+  } else {
+    alertaContainer.classList.remove('oculto');
+  }
+});
